1692d7d57104503d4b25f36a38835bdf34accb51
jdplab <jon@jon-polansky.com>
2024-06-11 13:41:29 -0400

    Added inital Terraform configuration

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+
+    backend "remote" {
+        organization = "JP-Lab"
+
+        workspaces {
+            name = "aws-website_dev"
+        }
+    }
+
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.53.0"
+        }
+    }
+}
+
+provider "aws" {
+    access_key = "var.AWS_ACCESS_KEY_ID"
+    secret_key = "var.AWS_SECRET_ACCESS_KEY"
+    token = "var.AWS_SESSION_TOKEN"
+    region = "var.region"
+}
+
+resource "aws_s3_bucket" "website" {
+    bucket = "jpolanskywebsite-dev"
+    
+    tags = {
+        Name = "jpolanskywebsite-dev"
+    }
+}
