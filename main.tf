f1ad6bf1b0a768dd3445f8e0f85d64bf703f10e1
jplab <jon@jon-polansky.com>
2024-06-12 19:21:13 -0400

    Modified variables

--- main.tf
+++ main.tf
@@ -17,10 +17,10 @@ terraform {
 }
 
 provider "aws" {
-    access_key = "var.AWS_ACCESS_KEY_ID"
-    secret_key = "var.AWS_SECRET_ACCESS_KEY"
-    token = "var.AWS_SESSION_TOKEN"
-    region = "var.region"
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
+    token = var.AWS_SESSION_TOKEN
+    region = var.region
 }
 
 resource "aws_s3_bucket" "website" {
--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
 variable "region" {
-  default     = "us-east-1"
+  type = string
+  default = "us-east-1"
 }
