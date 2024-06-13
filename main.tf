e3160a25fb37bed3ef5caa37b1c8af7e4e842c17
jplab <jon@jon-polansky.com>
2024-06-12 20:09:35 -0400

    Upload files

--- main.tf
+++ main.tf
@@ -30,3 +30,64 @@ resource "aws_s3_bucket" "website-dev" {
         Name = "jpolanskywebsite-dev"
     }
 }
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+    bucket = aws_s3_bucket.website-dev.id
+    rule {
+        object_ownership = "BucketOwnerPreferred"
+    }
+}
+
+resource "aws_s3_bucket_acl" "bucket-acl" {
+    bucket = aws_s3_bucket.website-dev.id
+    acl = "public-read"
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket-public-access-block" {
+    bucket = aws_s3_bucket.website-dev.id
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "bucket-policy" {
+    bucket = aws_s3_bucket.website-dev.id
+    policy = jsonencode({
+        Statement = [
+            {
+                Effect = "Allow",
+                Principal = "*",
+                Action = "s3:GetObject",
+                Resource = "${aws_s3_bucket.website-dev.arn}/*"
+            }
+        ]
+    })
+}
+
+resource "aws_s3_bucket_website_configuration" "website-config" {
+    bucket = aws_s3_bucket.website-dev.id
+    index_document {
+        suffix = "index.html"
+    }
+}
+
+resource "aws_s3_object" "html" {
+    foreach = fileset("web/", "*.html")
+    bucket = aws_s3_bucket.website-dev.id
+    key = each.value
+    source = "web/${each.value}"
+    content_type = "text/html"
+    etag = filemd5("web/${each.value}")
+    acl = "public-read"
+}
+
+resource "aws_s3_object" "css" {
+    foreach = fileset("web/", "*.css")
+    bucket = aws_s3_bucket.website-dev.id
+    key = each.value
+    source = "web/${each.value}"
+    content_type = "text/css"
+    etag = filemd5("web/${each.value}")
+    acl = "public-read"
+}
