03607e30bb8b29b11d0345875c492f0eabeae550
jplab <jon@jon-polansky.com>
2024-06-14 20:58:30 -0400

    Use ACM certificate

--- main.tf
+++ main.tf
@@ -18,11 +18,6 @@ terraform {
             source = "cloudflare/cloudflare"
             version = "4.25.0"
         }
-
-        acme = {
-            source = "vancluever/acme"
-            version = "2.21.0"
-        }
     }
 }
 
@@ -36,10 +31,6 @@ provider "cloudflare" {
   api_token = var.CLOUDFLARE_API_TOKEN
 }
 
-provider "acme" {
-  server_url = "https://acme-v02.api.letsencrypt.org/directory"
-}
-
 resource "aws_s3_bucket" "website-dev" {
     bucket = "jpolanskywebsite-dev"
     
@@ -48,32 +39,21 @@ resource "aws_s3_bucket" "website-dev" {
     }
 }
 
-resource "aws_s3_bucket_acl" "bucket-acl" {
-    bucket = aws_s3_bucket.website-dev.id
-    acl = "public-read"
-}
+data "aws_iam_policy_document" "bucket-policy" {
+    statement {
+        actions = ["s3:GetObject"]
+        resources = ["${aws_s3_bucket.website-dev.arn}/*"]
 
-resource "aws_s3_bucket_public_access_block" "bucket-public-access-block" {
-    bucket = aws_s3_bucket.website-dev.id
-    block_public_acls = false
-    block_public_policy = false
-    ignore_public_acls = false
-    restrict_public_buckets = false
+        principals {
+            type = "AWS"
+            identifiers = [aws_cloudfront_origin_access_identity.oai.iam_arn]
+        }
+    }
 }
 
 resource "aws_s3_bucket_policy" "bucket-policy" {
     bucket = aws_s3_bucket.website-dev.id
-    policy = jsonencode({
-        Version = "2012-10-17",
-        Statement = [
-            {
-                Effect = "Allow",
-                Principal = "*",
-                Action = "s3:GetObject",
-                Resource = "${aws_s3_bucket.website-dev.arn}/*"
-            }
-        ]
-    })
+    policy = data.aws_iam_policy_document.bucket-policy.json
 }
 
 resource "aws_s3_bucket_website_configuration" "website-config" {
@@ -84,33 +64,30 @@ resource "aws_s3_bucket_website_configuration" "website-config" {
     }
 }
 
-resource "tls_private_key" "private_key" {
-    algorithm = "RSA"
-}
-
-resource "acme_registration" "me" {
-    account_key_pem = tls_private_key.private_key.private_key_pem
-    email_address = var.EMAIL_ADDRESS
-}
-
-resource "acme_certificate" "cert" {
-    account_key_pem = acme_registration.me.account_key_pem
-    common_name = "aws.${var.DOMAIN_NAME}"
-    key_type = "2048"
-
-    dns_challenge {
-        provider = "cloudflare"
-        config = {
-            CF_ZONE_API_TOKEN = var.CLOUDFLARE_API_TOKEN
-            CF_DNS_API_TOKEN = var.CLOUDFLARE_API_TOKEN
-            CLOUDFLARE_HTTP_TIMEOUT = "300"
-        }
+resource "aws_acm_certificate" "cert" {
+    domain_name = "aws.${var.DOMAIN_NAME}"
+    validation_method = "DNS"
+    tags = {
+        Name = "jpolanskywebsite-dev"
     }
 }
 
-resource "aws_acm_certificate" "cert" {
-    private_key = acme_certificate.cert.private_key_pem
-    certificate_body = acme_certificate.cert.certificate_pem
+resource "aws_acm_certificate_validation" "cert-validation" {
+    certificate_arn = aws_acm_certificate.cert.arn
+    validation_record_fqdns = [for record in aws_acm_certificate.cert.domain_validation_options : record.resource_record_name]
+    depends_on = [ cloudflare_record.cert-validation ]
+}
+
+resource "cloudflare_record" "cert-validation" {
+    zone_id = var.CLOUDFLARE_ZONE_ID
+    name = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
+    value = aws_acm_certificate.cert.domain_validation_options[0].resource_record_value
+    type = "TXT"
+    proxied = false
+}
+
+resource "aws_cloudfront_origin_access_identity" "oai" {
+    comment = "OAI for jpolanskywebsite-dev"
 }
 
 resource "aws_cloudfront_distribution" "website" {
@@ -126,6 +103,10 @@ resource "aws_cloudfront_distribution" "website" {
     origin {
         domain_name = aws_s3_bucket.website-dev.bucket_regional_domain_name
         origin_id = aws_s3_bucket.website-dev.bucket_regional_domain_name
+    }
+
+    s3_origin_config {
+        origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
     }
     
     default_cache_behavior {
@@ -147,7 +128,7 @@ resource "aws_cloudfront_distribution" "website" {
     }
 
     viewer_certificate {
-        acm_certificate_arn = aws_acm_certificate.cert.arn
+        acm_certificate_arn = aws_acm_certificate_validation.cert-validation.certificate_arn
         ssl_support_method = "sni-only"
     }
 
