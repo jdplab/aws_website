0b749918aaaf44237bcb8e1fedc03398179ba53d
jplab <jon@jon-polansky.com>
2024-06-14 20:19:04 -0400

    Configure SSL and custom domain

--- main.tf
+++ main.tf
@@ -13,6 +13,16 @@ terraform {
             source = "hashicorp/aws"
             version = "5.53.0"
         }
+
+        cloudflare = {
+            source = "cloudflare/cloudflare"
+            version = "4.25.0"
+        }
+
+        acme = {
+            source = "vancluever/acme"
+            version = "2.23.0"
+        }
     }
 }
 
@@ -21,6 +31,14 @@ provider "aws" {
     secret_key = var.AWS_SECRET_ACCESS_KEY
     token = var.AWS_SESSION_TOKEN
     region = var.region
+}
+
+provider "cloudflare" {
+  api_token = var.CLOUDFLARE_API_TOKEN
+}
+
+provider "acme" {
+  server_url = "https://acme-v02.api.letsencrypt.org/directory"
 }
 
 resource "aws_s3_bucket" "website-dev" {
@@ -47,6 +65,7 @@ resource "aws_s3_bucket_public_access_block" "bucket-public-access-block" {
 resource "aws_s3_bucket_policy" "bucket-policy" {
     bucket = aws_s3_bucket.website-dev.id
     policy = jsonencode({
+        Version = "2012-10-17",
         Statement = [
             {
                 Effect = "Allow",
@@ -60,9 +79,93 @@ resource "aws_s3_bucket_policy" "bucket-policy" {
 
 resource "aws_s3_bucket_website_configuration" "website-config" {
     bucket = aws_s3_bucket.website-dev.id
+    
     index_document {
         suffix = "index.html"
     }
+}
+
+resource "tls_private_key" "private_key" {
+    algorithm = "RSA"
+}
+
+resource "acme_registration" "me" {
+    account_key_pem = tls_private_key.private_key.private_key_pem
+    email_address = var.EMAIL_ADDRESS
+}
+
+resource "acme_certificate" "cert" {
+    account_key_pem = acme_registration.me.account_key_pem
+    common_name = "aws.${var.DOMAIN_NAME}"
+    key_type = "2048"
+
+    dns_challenge {
+        provider = "cloudflare"
+        config = {
+            CF_ZONE_API_TOKEN = var.CLOUDFLARE_API_TOKEN
+            CF_DNS_API_TOKEN = var.CLOUDFLARE_API_TOKEN
+            CLOUDFLARE_HTTP_TIMEOUT = "300"
+        }
+    }
+}
+
+resource "aws_acm_certificate" "cert" {
+    private_key = acme_certificate.cert.private_key_pem
+    certificate_body = acme_certificate.cert.certificate
+}
+
+resource "aws_cloudfront_distribution" "website" {
+    enabled = true
+    is_ipv6_enabled = true
+    default_root_object = "index.html"
+    price_class = "PriceClass_100"
+    aliases = ["aws.${var.DOMAIN_NAME}"]
+    tags = {
+        Name = "jpolanskywebsite-dev"
+    }
+    
+    origin {
+        domain_name = aws_s3_bucket.website-dev.bucket_regional_domain_name
+        origin_id = aws_s3_bucket.website-dev.bucket_regional_domain_name
+    }
+    
+    default_cache_behavior {
+        allowed_methods = ["GET", "HEAD", "OPTIONS"]
+        cached_methods = ["GET", "HEAD", "OPTIONS"]
+        target_origin_id = aws_s3_bucket.website-dev.bucket_regional_domain_name
+        viewer_protocol_policy = "redirect-to-https"
+        min_ttl = 0
+        default_ttl = 3600
+        max_ttl = 86400
+        
+        forwarded_values {
+            query_string = false
+            
+            cookies {
+                forward = "none"
+            }
+        }
+    }
+
+    viewer_certificate {
+        acm_certificate_arn = aws_acm_certificate.cert.arn
+        ssl_support_method = "sni-only"
+    }
+
+    restrictions {
+        
+        geo_restriction {
+            restriction_type = "none"
+        }
+    }
+}
+
+resource "cloudflare_record" "cname" {
+    zone_id = var.CLOUDFLARE_ZONE_ID
+    name = "aws.${var.DOMAIN_NAME}"
+    value = aws_cloudfront_distribution.website.domain_name
+    type = "CNAME"
+    proxied = false
 }
 
 resource "aws_s3_object" "html" {
@@ -84,3 +187,4 @@ resource "aws_s3_object" "css" {
     etag = filemd5("web/${each.value}")
     acl = "public-read"
 }
+
--- variables.tf
+++ variables.tf
@@ -1,16 +1,33 @@
 variable "region" {
-  type = string
-  default = "us-east-1"
+    type = string
+    default = "us-east-1"
+}
+
+variable "DOMAIN_NAME" {
+    type = string
+    default = "jon-polansky.com"
 }
 
 variable "AWS_ACCESS_KEY_ID" {
-  type = string
+    type = string
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
-  type = string
+    type = string
 }
 
 variable "AWS_SESSION_TOKEN" {
-  type = string
+    type = string
+}
+
+variable "CLOUDFLARE_API_TOKEN" {
+    type = string
+}
+
+variable "CLOUDFLARE_ZONE_ID" {
+    type = string
+}
+
+variable "EMAIL_ADDRESS" {
+    type = string
 }
