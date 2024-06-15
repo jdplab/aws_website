713e8addf298f382191b158fd4f8c107bf6576d1
jplab <jon@jon-polansky.com>
2024-06-15 07:34:53 -0400

    Naming fix

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "aws_acm_certificate_validation" "cert-validation" {
 
 resource "cloudflare_record" "cert-validation" {
     for_each = {
-        for dvo in aws_acm_certificate.cert-validation.domain_validation_options : dvo.domain_name => {
+        for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
             name   = dvo.resource_record_name
             value = dvo.resource_record_value
             type   = dvo.resource_record_type
