c4a4c883bdeaf1c0aac8eb7926091df30a907c27
jdplab <jon@jon-polansky.com>
2024-07-08 15:28:38 -0400

    Modified API Gateway deployment

--- main.tf
+++ main.tf
@@ -321,6 +321,18 @@ resource "aws_api_gateway_deployment" "visitorcount_deployment" {
   depends_on = [aws_api_gateway_integration.visitorcount_integration]
   rest_api_id = aws_api_gateway_rest_api.visitorcount_api.id
   stage_name = "prod"
+
+  triggers = {
+    redeployment = sha256(
+      jsonencode({
+        rest_api = aws_api_gateway_rest_api.visitorcount_api.body
+      })
+    )
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_lambda_permission" "apigw_lambda" {
--- web/visitorcount.js
+++ web/visitorcount.js
@@ -1,7 +1,10 @@
 async function fetchVisitorCount() {
     const apiUrl = 'https://c73lqaarr4.execute-api.us-east-1.amazonaws.com/prod/visitorcount';
     try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(apiUrl, { mode: 'cors' });
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
         const data = await response.json();
         document.getElementById('visitorCount').textContent = data.visitorCount;
     } catch (error) {
