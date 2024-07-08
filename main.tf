54962c3a5d4b6785c3c081aa4a3f8a11d465a882
jdplab <jon@jon-polansky.com>
2024-07-08 12:55:18 -0400

    Added LambdaIntegration resource

--- main.tf
+++ main.tf
@@ -311,6 +311,15 @@ resource "aws_api_gateway_integration_response" "cors_options_integration_respon
   }
 }
 
+resource "aws_api_gateway_integration" "LambdaIntegration" {
+  rest_api_id = aws_api_gateway_rest_api.visitor_count_api.id
+  resource_id = aws_api_gateway_resource.VisitorCounterResource.id
+  http_method = aws_api_gateway_method.cors_options.http_method
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.visitor_counter.invoke_arn
+}
+
 resource "aws_api_gateway_deployment" "visitor_count_api_deployment" {
   depends_on = [
     aws_api_gateway_integration.LambdaIntegration,
