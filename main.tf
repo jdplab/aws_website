3544a20e47fbfcd30bc64e21a30d3aee7668ea7a
jdplab <jon@jon-polansky.com>
2024-07-08 12:14:16 -0400

    Added options method for API gateway

--- main.tf
+++ main.tf
@@ -274,21 +274,41 @@ resource "aws_api_gateway_resource" "VisitorCounterResource" {
   path_part = "visitorcount"
 }
 
-resource "aws_api_gateway_method" "VisitorCounterMethod" {
-  rest_api_id = aws_api_gateway_rest_api.visitor_count_api.id
-  resource_id = aws_api_gateway_resource.VisitorCounterResource.id
-  http_method = "GET"
+resource "aws_api_gateway_method" "cors_options" {
+  rest_api_id   = aws_api_gateway_rest_api.visitor_count_api.id
+  resource_id   = aws_api_gateway_resource.VisitorCounterResource.id
+  http_method   = "OPTIONS"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "LambdaIntegration" {
+resource "aws_api_gateway_method_response" "cors_options_response_200" {
   rest_api_id = aws_api_gateway_rest_api.visitor_count_api.id
   resource_id = aws_api_gateway_resource.VisitorCounterResource.id
-  http_method = aws_api_gateway_method.VisitorCounterMethod.http_method
-  
-  integration_http_method = "POST"
-  type = "AWS_PROXY"
-  uri = aws_lambda_function.visitor_counter.invoke_arn
+  http_method = aws_api_gateway_method.cors_options.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true,
+  }
+}
+
+resource "aws_api_gateway_integration_response" "cors_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.visitor_count_api.id
+  resource_id = aws_api_gateway_resource.VisitorCounterResource.id
+  http_method = aws_api_gateway_method.cors_options.http_method
+  status_code = aws_api_gateway_method_response.cors_options_response_200.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
 }
 
 resource "aws_api_gateway_deployment" "visitor_count_api_deployment" {
